c9e37628cbb828977f7cca854cf1333a65469c8b
Rawnak Hoque <Rawnak Hoque>
2017-03-15 17:01:24 -0700

    file arranged. resutls added.

--- Data/RNA-seq/Normal/code/RNASeq_normal.R
+++ Data/RNA-seq/Normal/code/RNASeq_normal.R
@@ -1,18 +1,19 @@
 
 library(gplots)
 library(NMF)
-library(DESeq2)
-library(limma)
-library(edgeR)
 
-setwd("C:/Users/rawna/Documents/GSAT-540-UBC/team_Bloodies/Data/RNA-seq/Normal")
+
+setwd("C:/Users/rawnak/Documents/team_Bloodies/Data/RNA-seq/Normal")
 expr_data = read.csv('GSE87195_rnaseq_ensT_all.csv', header = T,  sep = ',', row.names = 1)
+expr_data = expr_data[,c(1:10,12:13)]
 factor_data = read.csv('factor_data.csv', header = T, sep = ',')
 str(expr_data)
 #heatmap 
 cellType = factor(factor_data$cell_type, labels = c('CLP', 'GMP', 'HSC', 'HSC1','MLP', 'MLP1', 'MLP2',
-                                                    'MLP3', 'MPP', 'CMP', 'GMP2', 'MLP4', 'MPP1' ))
+                                                    'MLP3', 'MPP', 'CMP', 'MLP4', 'MPP1' ))
 aheatmap(cor(expr_data), Rowv = FALSE, Colv = FALSE, 
   annCol = list(Treatment = cellType))
-
+HSCvsMPP<- log2(expr_data[,1]/expr_data[,2])
+rownames(data$HSCvsMPP)
+MPPmean
 
