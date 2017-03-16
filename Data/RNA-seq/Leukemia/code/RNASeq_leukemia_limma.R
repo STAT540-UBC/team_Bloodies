6050eb17c87b17ba29157de8944b6a88030d5e63
Rawnak Hoque <Rawnak Hoque>
2017-03-15 18:24:47 -0700

    code corrected

--- Data/RNA-seq/Leukemia/code/RNASeq_leukemia_limma.R
+++ Data/RNA-seq/Leukemia/code/RNASeq_leukemia_limma.R
@@ -1,21 +1,15 @@
-library(limma)
+library("limma")
 library(edgeR)
-#setting working directiory
 setwd("C:/Users/rawnak/Documents/team_Bloodies/Data/RNA-seq/Leukemia")
-
-#reading files from two group of leukemia disease
 AML=read.table('AML/AML_expected_count_data.csv', sep = ',', header=TRUE, row.names=1)
 CLL=read.table('CLL/CLL_expected_count_data.csv',sep = ',', header=TRUE, row.names=1)
-
-#making the first column as rownames
 new_AML <- AML[,-1]
 rownames(new_AML) <- AML[,1]
 new_CLL <- CLL[,-1]
 rownames(new_CLL)<- CLL[,1]
 counts <- cbind(new_AML,new_CLL)
-#factorizing data
-groups <- factor(c(rep("AML", 7), c(rep("CLL", 7))))
 
+groups <- factor(c(rep("AML", 7), c(rep("CLL", 7))))
 
 # Build design matrix
 des <- model.matrix(~groups)
@@ -30,16 +24,12 @@ dat <- voom(counts, des, lib.size=lib.size)
 
 # Fit model
 fit <- lmFit(dat, des)
-#create an mean difference plot displaying the log-fold-changes and average A-values for each gene
-plotMD(fit)
-abline(0,0,col="blue")
-#Empirical Bayes analysis
+
 fit <- eBayes(fit)
 
 # Extract all results
-top.table1 <- topTable(fit, coef=2, n=Inf, p.value=0.001)
-#distribution of the p value
-hist(top.table1$P.Value,col="grey50", border="white")
+top.table1 <- topTable(fit, coef=2, n=Inf, p.value=0.05)
+
 write.table(top.table, file = "toptable.tsv", row.names=TRUE, col.names=NA, sep="\t")
 
 
